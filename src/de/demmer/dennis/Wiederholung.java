a94c2af33fdd5d61364d4d0f7dcbc29ef0c1b252
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-11-09 17:22:57 +0100

    initial commit

--- src/de/demmer/dennis/Wiederholung.java
+++ src/de/demmer/dennis/Wiederholung.java
@@ -2,10 +2,56 @@ package de.demmer.dennis;
 
 public class Wiederholung {
 	
+
+	public static void main(String[] args) {
+		
+		
+		int i = 2;
+		boolean b = true;
+		boolean b1 = false;
+		boolean b3 = 2 + 2 == 3;
+		
+		String s = "Hallo Welt";
+	
+		byte by = -127;
+		short sh = 1234;
+		long id = 2352623623623L;
+		
+		double d = 5234.234234;
+		float f = 235.234f;
+		
+		d = 5.5f;
+		f = (float) 5.5;
+			
+		
+		char c = 80;
+		char c1 = 'P';
+		
+		char c2 = '8';
+		char c3 = 56;
+		
+		System.out.println(c);
+		
+		
+		//häufigste Datentypen
+		/**
+		 * int
+		 * boolean
+		 * double
+		 * String
+		 * long
+		 */
+		
+		
+		
+	}
+	
+	
+	
 	
 	
 
 		
-	}
+
 
 }
